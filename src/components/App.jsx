7e236b4201c11475dea30e36fb2b6bd0d6a39b81
EvgenijRyabko <jeniaryabko@bk.ru>
2023-04-12 22:40:16 +0300

    postCssAdded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import Home from './Home/Home';
 import Login from './Login/Login';
 
 const App = () => {
-  return (
-    <div className="text-3xl font-bold underline">
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/home' element={<Home />} />
-      </Routes>
-    </div>
-  )
+	return (
+		<div className="flex bg-slate-300 items-center h-screen w-full justify-center">
+			<Routes>
+				<Route path='/' element={<Login />} />
+				<Route path='/home' element={<Home />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const navigation = useNavigate();
+	const navigation = useNavigate();
 
-    const signIn = (e) => {
-        e.preventDefault();
-        navigation('/home');
-    }
+	const signIn = (e) => {
+		e.preventDefault();
+		navigation('/home');
+	}
 
-    return (
-        <div className="text-3xl font-bold underline">
-            <input id='login' />
-            <label for='login'>Login</label>
-            <input id='password' />
-            <label for='password'>Password</label>
-            <button type='submit' onClick={signIn}>Sign in</button>
-        </div>
-    )
+	return (
+		<div className="grid bg-white shadow-md shadow-slate-100 bg-slate-100 rounded-xl w-9/12 h-3/4 items-center justify-center">
+			<div>
+				<input id='login' className="w-2/6 justify-center justify-self-center" />
+				<label for='login'>Login</label>
+			</div>
+			<div>
+				<input id='password' className="w-2/6" />
+				<label for='password'>Password</label>
+			</div>
+			<button type='submit' onClick={signIn}>Sign in</button>
+		</div>
+	)
 }
 
 export default Login;
