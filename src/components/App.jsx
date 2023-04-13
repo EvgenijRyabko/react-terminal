595a461a68460c4d8baa7f7b7170a9cc70d1a033
Evgenij Ryabko <jeniaryanko@mail.ru>
2023-04-13 15:11:40 +0300

    LoginHalfStyled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
 import Login from './Login/Login';
+import classes from './App.module.css'
 
 const App = () => {
 	return (
-		<div className="flex bg-slate-300 items-center h-screen w-full justify-center">
+		<div className={ classes.app }>
 			<Routes>
 				<Route path='/' element={<Login />} />
 				<Route path='/home' element={<Home />} />
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,60 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import classes from './Login.module.css';
 
 const Login = () => {
-	const navigation = useNavigate();
+  const navigation = useNavigate();
 
-	const signIn = (e) => {
-		e.preventDefault();
-		navigation('/home');
-	}
+  const signIn = (e) => {
+    e.preventDefault();
+    navigation("/home");
+  };
 
-	return (
-		<div className="grid bg-white shadow-md shadow-slate-100 bg-slate-100 rounded-xl w-9/12 h-3/4 items-center justify-center">
-			<div>
-				<input id='login' className="w-2/6 justify-center justify-self-center" />
-				<label for='login'>Login</label>
-			</div>
-			<div>
-				<input id='password' className="w-2/6" />
-				<label for='password'>Password</label>
-			</div>
-			<button type='submit' onClick={signIn}>Sign in</button>
-		</div>
-	)
-}
+  return (
+    <div className={ classes.loginForm }>
+      <div className="grid content-end row-auto gap-y-12">
+        <div className="grid w-full justify-items-center">
+          <div className="relative w-3/6 z-0">
+            <input
+              name="login"
+              type='text'
+              className="peer px-2 w-full min-h-[40px] border-2 rounded-xl outline-none bg-transparent focus:bg-white focus:border-blue-600 transform duration-500"
+            />
+            <label
+              for="login"
+              className="absolute text-slate-700 left-4 top-1 peer-hover:top-[-30px] peer-focus:top-[-30px] peer-focus:text-blue-600 transform duration-500 -z-10"
+            >
+              Login
+            </label>
+          </div>
+        </div>
+        <div className="grid w-full justify-items-center">
+          <div className="relative w-3/6 z-0">
+            <input
+              name="password"
+              type='password'
+              className="peer px-2 w-full min-h-[40px] border-2 rounded-xl outline-none bg-transparent focus:bg-white focus:border-blue-600 transform duration-500"
+            />
+            <label
+              for="password"
+              className="absolute text-slate-700 left-4 top-1 peer-hover:top-[-30px] peer-focus:top-[-30px] peer-focus:text-blue-600 transform duration-500 -z-10"
+            >
+              Password
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="grid items-center w-full justify-items-center">
+        <button
+          className="w-1/4 h-[40px] text-slate-100 bg-amber-500 rounded-md shadow-amber-500 shadow-md hover:opacity-90 hover:shadow-[0_5px_20px_#f59e0b] transition duration-500 justify-self-center"
+          type="submit"
+          onClick={signIn}
+        >
+          Sign in
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
